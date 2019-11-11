ba1a47918bb7410a54820b186487151567fa6c72
tOlheiser <tolheiser@outlook.com>
2019-11-11 13:34:18 -0600

    Finished logic for the Header Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+1. The Header component contains a form field. User submit a task.
+2. Submitted task gets lifted to the parent component, App, and App passes that data to the Body component.
+3. You setState on the App component based on the previous state, appending the task as an object to the App component's state. The App passes down the tasks as props to the Body component.
+The object contains the 'description' and a 'status' keys. 
+4. You map over the tasks in the Body's props and create list items. (I think this is where destructuring in the parameter comes into play)
+- These list items have the description, status, and delete functionality. Clicking on them will toggle the status.
+5. The Footer does three things:
+- Displays the amount of 'Active' tasks remain.
+- Provides filter functionality 'All', 'Active', 'Completed'
+- Button to 'Clear Completed'
+*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* Unsure of:
+1. Unclear on how to grab the data when you submit the form. 
+2. Lifting state. Creating a diagram to visualize the flow of data would be a great learning tool. 
+3. 
+*/
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            tasks: [],
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value,
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        
+        // store the user input.
+        const description = this.state.value;
+
+        this.setState(({tasks}) => {
+            return {
+                value: '', // reset the input.
+                tasks: [...tasks, {
+                    description,
+                    status: 'Active'
+                }]
+            }
+        }) 
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Hello Kevin <br/>
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
+
+
